651fc3cfd6684951649122abf7e168c4120dc550
Chen Feng <puck.chen@foxmail.com>
2016-10-12 19:02:39 +0800

    Create pc.py

---
+++ draw/pc.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+class windows:
+    def cmd(self, cmd):
+        try:
+            ret = os.popen(cmd).readlines()
+        except:
+            print "exec error"
+        return "".join(ret).replace('\r', '') #return a string
+    
+    def exec_bat(self, bat_path):
+        cur_dir = os.getcwd()
+        bat = os.path.basename(bat_path)
+        image_dir = os.path.dirname(bat_path)
+        os.chdir(image_dir)
+        bat_run = subprocess.Popen(bat, stdin=subprocess.PIPE)
+        bat_run.communicate("")
+        self.cmd("adb wait-for-device")
+        os.chdir(cur_dir)
+
+class linux:
+        def cmd(self, cmd):
+            "TODO"
+            
+            
+# if __name__ == '__main__':
+#     local_windows = windows()
+#     ret = local_windows.cmd("adb shell cat /proc/meminfo")
+#     print ret.split()
